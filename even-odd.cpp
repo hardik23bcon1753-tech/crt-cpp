5952c1a72f6be0af2e61c983f62811216c860d1d
hardik23bcon1753-tech <hardik.23bcon1753@jecrcu.edu.in>
2026-05-01 11:35:01 +0530

    init

---
+++ even-odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+ int main () {
+ int num ;
+
+ cout<<"enter a num ";
+
+ cin>> num;
+
+ if ("num % 2 ==0") {
+ cout<< "even";
+ }else {
+  cout<<"odd";
+ }
+
+  return 0;
+}
